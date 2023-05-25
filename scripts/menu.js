6c950b7d7d8590c61c3c5a8078ec4c3576178146
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-05-25 21:00:29 +0300

    add mobile version

---
+++ scripts/menu.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("ul").style.width = "250px";
+    document.getElementById("ul").style.opacity = "100";
+}
+
+function closeNav() {
+    document.getElementById("ul").style.width = "0";
+    document.getElementById("ul").style.opacity = "0";
+}
+
+
+
